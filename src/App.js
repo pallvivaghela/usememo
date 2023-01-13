9ffd152ce0e95d1603ab39eb478931fdcb4d6d82
raju <anupchopde06@gmail.com>
2023-01-13 11:09:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState , useMemo } from 'react';
 
 function App() {
+  const [cnt, setCnt] = useState(0);
+  const [item, setItem] = useState(0);
+
+  const demo = useMemo( () => function test(){
+    // setCnt((c) => c + 1);
+    console.log(cnt);
+    // alert();
+  })(cnt)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {demo}
+      <input type="button" value="setCnt" onClick={() => setCnt(cnt + 1)}></input>
+      <input type="button" value="setItem" onClick={() => setItem(item + 1)}></input>
     </div>
   );
 }
